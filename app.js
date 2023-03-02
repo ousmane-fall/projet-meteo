c721967c61dac944449b717cedd727e95f27405f
ousmane-fall <fallousman06@gmail.com>
2023-03-02 12:58:58 +0100

    commit meteo

---
+++ app.js
@@ -0,0 +1,52 @@
+// Vérification de la compatibilité avec les Service Workers
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => console.log('ServiceWorker registration successful with scope: ', registration.scope))
+      .catch(err => console.log('ServiceWorker registration failed: ', err));
+  });
+}
+
+// Récupération des données météorologiques de Paris
+fetch('https://api.openweathermap.org/data/2.5/forecast?q=Paris&units=metric&appid=367b4be22d94816898377fabc9fc9de9')
+  .then(response => response.json())
+  .then(data => {
+    const weatherData = data.list.filter(item => item.dt_txt.includes('12:00:00'));
+    const weatherContainer = document.getElementById('weather-container');
+
+    // Création des cartes météo
+    weatherData.forEach(item => {
+      const { dt, main: { temp }, weather: [ { description } ] } = item;
+      const day = new Date(dt * 1000).toLocaleDateString('fr-FR', { weekday: 'long' });
+      const card = document.createElement('div');
+      card.classList.add('weather-card');
+      card.innerHTML = `
+        <h2>${day}</h2>
+        <p>${Math.round(temp)}°C</p>
+        <p>${description}</p>
+      `;
+      card.addEventListener('click', () => createDetailedCard(item));
+      weatherContainer.appendChild(card);
+    });
+  })
+  .catch(error => console.error(error));
+
+function createDetailedCard(data) {
+  const { dt, main: { temp, feels_like, humidity, pressure }, weather: [ { description, icon } ] } = data;
+  const day = new Date(dt * 1000).toLocaleDateString('fr-FR', { weekday: 'long' });
+  const card = document.createElement('div');
+  card.classList.add('detailed-card');
+  card.innerHTML = `
+    <h2>${day}</h2>
+    <p><img src="https://openweathermap.org/img/w/${icon}.png" alt="${description}"> ${description}</p>
+    <p>Temperature : ${Math.round(temp)}°C</p>
+    <p>Ressenti : ${Math.round(feels_like)}°C</p>
+    <p>Humidite : ${humidity}%</p>
+    <p>Pression : ${pressure} hPa</p>
+  `;
+  const closeButton = document.createElement('button');
+  closeButton.textContent = 'Fermer';
+  closeButton.addEventListener('click', () => card.remove());
+  card.appendChild(closeButton);
+  document.body.appendChild(card);
+}
